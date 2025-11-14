48491afa97b30212439e3b114062d706ff1a23d8
Fab-uleux <fabien.turgeon@proton.me>
2025-11-14 11:11:37 -0500

    ROT13_Code works but results in out of bound if index is above 16

--- src/ROT13_Code.java
+++ src/ROT13_Code.java
@@ -2,16 +2,24 @@ public class ROT13_Code {
 
     public String rot13(String entree) {
         char[] tab = entree.toCharArray();
+        char[] alphabet = "abcdefghijklmnopqrstuvwxyz".toCharArray();
+        char[] alphabetMaj = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".toCharArray();
         System.out.println("Rot13");
 
         for (int i = 0; i < tab.length; i++) {
-            for (int j = i + 1; j < tab.length; j++) {
-                char temp = tab[i];
-            }
-        }
+            char charactere = tab[i];
 
-        for (int i = 0; i < tab.length; i++) {
-            System.out.print(tab[i]);
+            for (int j = 0; j < alphabet.length; j++) {
+                if (charactere == alphabet[j]) {
+                    tab[i] = alphabet[j + 13];
+                }
+            }
+
+            for (int j = 0; j < alphabetMaj.length; j++) {
+                if (charactere == alphabetMaj[j]) {
+                    tab[i] = alphabetMaj[j + 13];
+                }
+            }
         }
 
         System.out.println();
